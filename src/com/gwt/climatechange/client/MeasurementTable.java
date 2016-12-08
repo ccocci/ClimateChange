edc7afa887cc7f63954a6cf8bd6d1f9447195d8b
ryublue <sophy.chhong@hispeed.ch>
2016-12-08 12:44:51 +0100

    removed Latitude & Longitude from the table

--- src/com/gwt/climatechange/client/MeasurementTable.java
+++ src/com/gwt/climatechange/client/MeasurementTable.java
@@ -15,8 +15,8 @@ public class MeasurementTable {
  		dataFlexTable.setText(0, 2, "Average Temperature Uncertainty");
  		dataFlexTable.setText(0, 3, "City");
  		dataFlexTable.setText(0, 4, "Country");
- 		dataFlexTable.setText(0, 5, "Latitude");
- 		dataFlexTable.setText(0, 6, "Longitude");
+ 		//dataFlexTable.setText(0, 5, "Latitude");
+ 		//dataFlexTable.setText(0, 6, "Longitude");
  		
  		// Add styles to elements in the measurement table.
  		dataFlexTable.getRowFormatter().addStyleName(0, "tableHeader");
@@ -43,16 +43,16 @@ public class MeasurementTable {
 		
 		Float avgTemperature = new Float(Math.round(dataPoint.getTemperature()));
 		Float uncertainty = new Float(Math.round(dataPoint.getUncertainty()));
-		Float latitude = new Float(Math.round(dataPoint.getLatitude()));
-		Float longitude = new Float(Math.round(dataPoint.getLongitude()));
+		//Float latitude = new Float(Math.round(dataPoint.getLatitude()));
+		//Float longitude = new Float(Math.round(dataPoint.getLongitude()));
 		
 		dataFlexTable.setText(row, 0, DateTimeFormat.getFormat("dd/MM/yyyy").format(dataPoint.getDate()));
 		dataFlexTable.setText(row, 1, avgTemperature.toString());
 		dataFlexTable.setText(row, 2, uncertainty.toString());
 		dataFlexTable.setText(row, 3, dataPoint.getCity());
 		dataFlexTable.setText(row, 4, dataPoint.getCountry());
-		dataFlexTable.setText(row, 5, latitude.toString());
-		dataFlexTable.setText(row, 6, longitude.toString());
+		//dataFlexTable.setText(row, 5, latitude.toString());
+		//dataFlexTable.setText(row, 6, longitude.toString());
 		
 		for(int i = 0; i < measurementNumberOfColumns; i++){
 			dataFlexTable.getCellFormatter().addStyleName(row, i, "filterTableColumn");
